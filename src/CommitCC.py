69f0defc787e3c4b4c3beeee679235c771d5dd39
Diracnote <diracnote@gmail.com>
2013-11-18 11:23:05 +0800

    修正提交操作，添加main函数

--- src/CommitCC.py
+++ src/CommitCC.py
@@ -12,6 +12,9 @@ from os.path import dirname
 from os.path import isdir
 from os.path import relpath
 from os import mkdir
+import configparser
+from os import sep
+import os
 
 class CommitCC(object):
     '''
@@ -26,45 +29,55 @@ class CommitCC(object):
         self.local = local
         self.remote = remote
         for line in diff.new:
-            print(" ".join(["NEW:", "/".join(str(line))]))
+            print(" ".join(["NEW:", sep.join(line)]))
             self.__commitNew(line)
         for line in diff.gone:
-            print(" ".join(["GONE:", "/".join(str(line))]))
+            print(" ".join(["GONE:", sep.join(line)]))
             self.__commitDel(line)
         for line in diff.mod:
             print(" ".join(["MOD:", str(line)]))
             self.__commitMod(line)
             
     def __commitNew(self, rc=("", "")):
-        relpath = relpath("/".join(rc), self.local)
-        remote_file = "/".join([self.remote, relpath])
+        localfile = sep.join(rc)
+        relpath = os.path.relpath(localfile, self.local)
+        remotefile = "/".join([self.remote, relpath])
         # 在CC上创建文件父目录
-        remote_dir = dirname(remote_file)
+        remote_dir = dirname(remotefile)
         while not isdir(remote_dir):
-            mid_dir = remote_dir
-            while not isdir(dirname(mid_dir)):
-                mid_dir = dirname(mid_dir)
-            mkdir(mid_dir)
-            self.cc.checkout(dirname(mid_dir))
-            self.cc.mkdir(mid_dir)
-            self.cc.checkin(dirname(mid_dir))
-        self.cc.checkout(dirname(remote_file))
-        self.cc.mkelem(remote_file)
-        self.cc.checkin(dirname(remote_file))
+            middir = remote_dir
+            while not isdir(dirname(middir)):
+                middir = dirname(middir)
+            mkdir(middir)
+            self.cc.checkout(dirname(middir))
+            self.cc.mkdir(middir)
+            self.cc.checkin(dirname(middir))
+        if self.cc.checkout(dirname(remotefile)) == 0:
+            copyfile(localfile, remotefile)
+            self.cc.mkelem(remotefile)
+        self.cc.checkin(dirname(remotefile))
     
     def __commitDel(self, rc=("", "")):
         path = rc[0]
-        file = "/".join(rc)
+        file = sep.join(rc)
         self.cc.checkout(path)
         self.cc.rm(file)
-        self.cc.checkin(dir)
+        self.cc.checkin(path)
     
     def __commitMod(self, rc=("", "")):
         local_file, remote_file = rc
-        self.cc.checkout(remote_file)
-        copyfile(local_file, remote_file)
+        if self.cc.checkout(remote_file) == 0:
+            copyfile(local_file, remote_file)
         self.cc.checkin(remote_file)
     
 if __name__ == "__main__":
-    #TODO: 通过config处理CC提交
-    pass
+    config = configparser.ConfigParser()
+    config.optionxform = str
+    config_file = "config.ini"
+    config.read(config_file)
+    local = config.get("REPO", "LOCAL")
+    remote = config.get("REPO", "CC")
+    print("".join(["本地代码：", local]))
+    print("".join(["CC代码：", remote]))
+    cc = CommitCC()
+    cc.commit(local, remote)
